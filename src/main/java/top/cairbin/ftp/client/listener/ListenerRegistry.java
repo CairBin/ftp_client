c5a42c783fbd5caf7241948e39db6b98d2d4083d
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-10-22 00:10:01 +0800

    feat: 为监听器添加依赖注入

--- src/main/java/top/cairbin/ftp/client/listener/ListenerRegistry.java
+++ src/main/java/top/cairbin/ftp/client/listener/ListenerRegistry.java
@@ -2,29 +2,41 @@
  * @Description: 自动注册Listener
  * @License: MIT License
  * @Author: Xinyi Liu(CairBin)
- * @version: 
+ * @version: 1.0.0
  * @Date: 2024-10-14 02:16:08
  * @LastEditors: Xinyi Liu(CairBin)
- * @LastEditTime: 2024-10-14 02:39:30
+ * @LastEditTime: 2024-10-22 00:08:24
  * @Copyright: Copyright (c) 2024 Xinyi Liu(CairBin)
  */
 package top.cairbin.ftp.client.listener;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+
 import org.reflections.Reflections;
 
 import com.google.inject.Inject;
 
+import top.cairbin.ftp.InjectorFactory;
+import top.cairbin.ftp.logger.ILogger;
+
 public class ListenerRegistry implements IListenerRegistry{
-    private IEventSource eventSource;
+    @Inject
+    private ILogger logger;
+
+    
+    private final IEventSource eventSource;
     
     @Inject
     public ListenerRegistry(IEventSource eventSource) {
         this.eventSource = eventSource;
     }
 
-    // 注册所有带有 @Listener 注解的类
+    /**
+     * @description: 注册所有带有 @Listener 注解的类
+     * @param {String} packageName 待扫描的包名
+     */    
+    @Override
     public void registerListeners(String packageName) {
         Reflections reflections = new Reflections(packageName);  // 使用 Reflections 库来扫描类
 
@@ -35,12 +47,14 @@ public class ListenerRegistry implements IListenerRegistry{
             try {
                 // 创建实例并注册为监听器
                 Object instance = clazz.getDeclaredConstructor().newInstance();
-                if (instance instanceof ICustomEventListener) {
+                if(instance instanceof ICustomEventListener) {
+                    InjectorFactory.getInjector().injectMembers(instance);  // 依赖注入
                     eventSource.registerListener((ICustomEventListener) instance);
-                    System.out.println("Registered listener: " + clazz.getSimpleName());
+                    logger.debug("Registered listener: " + clazz.getSimpleName());
                 }
             } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
                 e.printStackTrace();
+                logger.error("[ListenerRegistry] Error registering listener " + e);
             }
         }
     }
